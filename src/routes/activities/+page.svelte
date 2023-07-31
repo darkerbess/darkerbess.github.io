1771b9057035d9e650f119f9bc15942c11de9db2
darkerbess <towndiversion6@gmail.com>
2023-07-30 23:29:20 -0500

    some changes

--- src/routes/activities/+page.svelte
+++ src/routes/activities/+page.svelte
@@ -42,7 +42,7 @@
   .is-hero-image {
     position: relative;
     background: linear-gradient(to top, #009739aa, #009739aa),
-      url("https://darkerbess.github.io/_app/immutable/assets/activities.ea487385.jpg") no-repeat center center;
+      url("https://github.com/darkerbess/darkerbess.github.io/blob/master/src/lib/assets/activities.jpg?raw=true") no-repeat center center;
     background-size: cover;
   }
   .hero-body {
