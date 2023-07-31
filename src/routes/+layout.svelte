838c37276bffbdd038c63bdb9784fca3c4ce3001
darkerbess <towndiversion6@gmail.com>
2023-07-30 20:41:20 -0500

    some changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,4 @@
 <script>
-  import "@fontsource/poppins/400.css";
-  import "@fontsource/poppins/700.css";
-  import "@fontsource/poppins/900.css";
   import "../app.scss";
   import "bulma/bulma.sass";
 </script>
